567846355b399f716fdea7bb98e0808a28b1ab42
poltman <poltman@onet.pl>
2023-11-11 20:52:28 +0100

    correction

--- spring/webflux/webflux-postgresql-example-01/src/main/resources/schema.sql
+++ spring/webflux/webflux-postgresql-example-01/src/main/resources/schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS organization;
 CREATE TABLE IF NOT EXISTS organization (id SERIAL PRIMARY KEY, name VARCHAR(255), description VARCHAR(255), status BOOLEAN);
-insert into organization(id, name, description, status) values(1, 'organization#1name', 'descr#1', 'true');
-insert into organization(id, name, description, status) values(2, 'organization#2name', 'descr#2', 'true');
+insert into organization(name, description, status) values('organization#1name', 'descr#1', 'true');
+insert into organization(name, description, status) values('organization#2name', 'descr#2', 'true');
+insert into organization(name, description, status) values('organization#3name', 'descr#3', 'true');
