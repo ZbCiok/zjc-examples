d8bfdff1663ee68e3ba311027042ff130c1885df
poltman <poltman@onet.pl>
2023-11-12 15:39:18 +0100

    corrections

--- spring/webflux/webflux-r2dbc-pagination/src/main/java/zjc/examples/reactive/r2dbc/R2dbcPaginationApplication.java
+++ spring/webflux/webflux-r2dbc-pagination/src/main/java/zjc/examples/reactive/r2dbc/R2dbcPaginationApplication.java
@@ -1,6 +1,5 @@
 package zjc.examples.reactive.r2dbc;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -15,7 +14,6 @@ import zjc.examples.reactive.r2dbc.entity.Product;
 
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ThreadLocalRandom;
-
 
 @SpringBootApplication
 public class R2dbcPaginationApplication  implements CommandLineRunner {
