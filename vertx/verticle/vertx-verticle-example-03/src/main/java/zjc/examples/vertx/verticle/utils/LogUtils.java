7dbdd42dba430145feb964c836257dd56b4bca42
poltman <poltman@onet.pl>
2023-11-23 13:48:29 +0100

    verticle-example 03 02

--- vertx/verticle/vertx-verticle-example-03/src/main/java/zjc/examples/vertx/verticle/utils/LogUtils.java
+++ vertx/verticle/vertx-verticle-example-03/src/main/java/zjc/examples/vertx/verticle/utils/LogUtils.java
@@ -2,14 +2,8 @@ package zjc.examples.vertx.verticle.utils;
 
 public enum LogUtils {
 
-    REGULAR_CALL_SUCCESS_MESSAGE("%s called w/ success - %s"),
-    REGULAR_CALL_ERROR_MESSAGE("%s called w/ error - %s"),
-    NO_BOOK_WITH_ID_MESSAGE("No book with id %d"),
-    CANNOT_CREATE_BOOK_MESSAGE("Cannot create a new book"),
     RUN_HTTP_SERVER_SUCCESS_MESSAGE("HTTP server running on port %s"),
-    RUN_HTTP_SERVER_ERROR_MESSAGE("Cannot run HTTP server"),
-    NULL_OFFSET_ERROR_MESSAGE("Offset can't be null. Page %s and limit %s"),
-    RUN_APP_SUCCESSFULLY_MESSAGE("vertx-4-reactive-rest-api started successfully in %d ms");
+    RUN_HTTP_SERVER_ERROR_MESSAGE("Cannot run HTTP server");
 
     private final String message;
 
