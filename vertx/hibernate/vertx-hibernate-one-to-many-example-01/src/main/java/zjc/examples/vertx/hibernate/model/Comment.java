c7fc2350a4ad992fc260289bc5880e2e1c031e7c
poltman <poltman@onet.pl>
2023-12-17 14:09:25 +0100

    Corrections Comment.java 01

--- vertx/hibernate/vertx-hibernate-one-to-many-example-01/src/main/java/zjc/examples/vertx/hibernate/model/Comment.java
+++ vertx/hibernate/vertx-hibernate-one-to-many-example-01/src/main/java/zjc/examples/vertx/hibernate/model/Comment.java
@@ -1,9 +1,6 @@
 package zjc.examples.vertx.hibernate.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 import static jakarta.persistence.FetchType.LAZY;
@@ -18,6 +15,7 @@ public class Comment {
   private String content;
 
   @NotNull
+  @JoinColumn(name = "product_id", nullable = false)
   @ManyToOne(fetch = LAZY)
   private Product product;
 
