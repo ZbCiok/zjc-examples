23404600b2a676f0687143d2e9dc2b1306431b0b
poltman <poltman@onet.pl>
2024-01-11 13:06:57 +0100

    first commit

---
+++ quarkus/panache/quarkus-hibernate-orm-panache-quickstart/src/test/java/zjc/examples/quarkus/hibernate/orm/panache/quickstart/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package zjc.examples.quarkus.hibernate.orm.panache.quickstart;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
