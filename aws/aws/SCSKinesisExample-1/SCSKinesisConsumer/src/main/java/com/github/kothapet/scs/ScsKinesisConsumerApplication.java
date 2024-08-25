d67c2194127fa6af905b9952d14a749233d9c17b
zbyszek <poltman@onet.pl>
2024-08-25 20:55:24 +0200

    logging 01

--- aws/aws/SCSKinesisExample-1/SCSKinesisConsumer/src/main/java/com/github/kothapet/scs/ScsKinesisConsumerApplication.java
+++ aws/aws/SCSKinesisExample-1/SCSKinesisConsumer/src/main/java/com/github/kothapet/scs/ScsKinesisConsumerApplication.java
@@ -1,12 +1,18 @@
 package com.github.kothapet.scs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class ScsKinesisConsumerApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(ScsKinesisConsumerApplication.class);
+		logger.info("ScsKinesisConsumerApplication Start");
 		SpringApplication.run(ScsKinesisConsumerApplication.class, args);
 	}
 
--- aws/aws/SCSKinesisExample-1/SCSKinesisConsumer/src/main/java/com/github/kothapet/scs/controller/KinesisController.java
+++ aws/aws/SCSKinesisExample-1/SCSKinesisConsumer/src/main/java/com/github/kothapet/scs/controller/KinesisController.java
@@ -2,6 +2,9 @@ package com.github.kothapet.scs.controller;
 
 import java.util.function.Consumer;
 
+import com.github.kothapet.scs.ScsKinesisConsumerApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
@@ -16,17 +19,18 @@ import com.google.common.base.Supplier;
 @Configuration
 @Controller
 public class KinesisController {
-	
+	Logger logger = LoggerFactory.getLogger(KinesisController.class);
+
 	@Autowired
 	StreamBridge streamBridge;
 	
 	@Bean
 	Consumer<Message<IntegerEntity>> myConsumer() {
 		return message -> {
-			System.out.println("Consumer received Header  : " + message.getHeaders());
+			logger.info("Consumer received Header  : " + message.getHeaders());
 			//Integer partKey = (Integer) message.getHeaders().get(KinesisHeaders);
 			IntegerEntity payload = (IntegerEntity) message.getPayload();
-			System.out.println(" ###### Consumer ###### received payload : " + payload);
+			logger.info(" ###### Consumer ###### received payload : " + payload);
 		};
 	}
 
