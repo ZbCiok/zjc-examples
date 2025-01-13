33fec68b9c932c87ab5bc849f70eedd8d216cd30
zbyszek <poltman@onet.pl>
2025-01-13 14:39:54 +0100

    rabbitmq-getting-started 01

---
+++ reactive-messaging/rabbitmq/rabbitmq-getting-started/src/main/java/org/example/CommonConfigs.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class CommonConfigs {
+  public static final String DEFAULT_QUEUE = "Queue-Getting-Started";
+  public static final String AMQP_URL = "amqp://guest:guest@localhost:5672/";
+}
